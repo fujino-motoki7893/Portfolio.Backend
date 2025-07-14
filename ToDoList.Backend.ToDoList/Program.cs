f5a150952d00a779715ec5af9d6b1c5a1136283a
Motoki Fujino <103023506+fujino-motoki22-fixer@users.noreply.github.com>
2025-07-14 13:58:12 +0900

    ✨ Program.cs を名前空間で括った (#20)

--- ToDoList.Backend.ToDoList/Program.cs
+++ ToDoList.Backend.ToDoList/Program.cs
@@ -2,41 +2,57 @@ using Microsoft.EntityFrameworkCore;
 using ToDoList.Backend.ToDoList.Controllers;
 using ToDoList.Backend.ToDoList.Models.DbContexts;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// サービスの登録
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite("Data Source=conferences.db"));
-
-// REST API用のサービスを追加
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services
-    .AddGraphQLServer()
-    .AddQueryType<Query>()
-    .AddMutationType<Mutation>();
-
-var app = builder.Build();
-
-// Swagger設定（開発環境のみ）
-if (app.Environment.IsDevelopment())
+namespace ToDoList.Backend.ToDoList
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    /// <summary>
+    /// プログラムのエントリーポイント
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// メインメソッド
+        /// </summary>
+        /// <param name="args">コマンドライン引数</param>
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // サービスの登録
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite("Data Source=conferences.db"));
+
+            // REST API用のサービスを追加
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services
+                .AddGraphQLServer()
+                .AddQueryType<Query>()
+                .AddMutationType<Mutation>();
+
+            var app = builder.Build();
+
+            // Swagger設定（開発環境のみ）
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            // ルーティングのミドルウェアを追加
+            app.UseRouting();
+
+            // エンドポイントのミドルウェアを設定
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGraphQL();
+                endpoints.MapControllers(); // REST APIコントローラーをマップ
+            });
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
 }
-
-// ルーティングのミドルウェアを追加
-app.UseRouting();
-
-// エンドポイントのミドルウェアを設定
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapGraphQL();
-    endpoints.MapControllers(); // REST APIコントローラーをマップ
-});
-
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
